26264e90d02b821b5cd4ef9c06d57886b805e5d7
olowo28 <olowoporokuvictor1@gmail.com>
2025-11-13 16:23:33 +0100

    added configuration for vercel

--- project1/settings.py
+++ project1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-9ne1ha*^a!!&1muv@#it8vb*x9w*kmon7r-l7^v^!mht@%!n9v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh','127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -77,8 +78,15 @@ WSGI_APPLICATION = 'project1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD':'GEOSGtNpjxdPWVBjERyMufsoyoWLFLaN',
+        'HOST': 'metro.proxy.rlwy.net',
+        'PORT': '29732',
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
 
@@ -118,10 +126,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS =[BASE_DIR / 'static'] # Local static files (during development)
 
-STATICFILES_DIRS =[
-    BASE_DIR / 'static'
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')# Folder where collected static files will be stored during build/deploy
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
